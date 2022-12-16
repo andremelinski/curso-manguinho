0aeff7bac833b76f07ffbb288b095826341d0216
andremelinski <andremelinski29@gmail.com>
2022-12-16 17:17:16 -0300

    fix: husky

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,6 +1,7 @@
 import { HttpReponse, HttpRequest } from '../http/http';
 
 export class SignUpController {
+	// eslint-disable-next-line class-methods-use-this
 	handle(httRequest: HttpRequest): HttpReponse {
 		if (!httRequest.body.name) {
 			return { statusCode: 400, body: new Error('Missing param: name') };
@@ -8,6 +9,6 @@ export class SignUpController {
 		if (!httRequest.body.email) {
 			return { statusCode: 400, body: new Error('Missing param: email') };
 		}
-		return { statusCode: 200, body: 'ok' };
+		return { statusCode: 200, body: 'ok!' };
 	}
 }
