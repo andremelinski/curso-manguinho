ba23847efd0c30bbc26515bf142a1fe04c74f8fb
andremelinski <andremelinski29@gmail.com>
2022-12-16 16:33:18 -0300

    adding http interfaces

--- src/presentation/controllers/signuip.spec.ts
+++ src/presentation/controllers/signuip.spec.ts
@@ -16,4 +16,19 @@ describe('Sign Up Controller', () => {
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new Error('Missing param: name'));
 	});
+	test('Should return 400 if no email is provided', () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: 'andre',
+				password: 'a123',
+				passwordConfirmation: 'a123',
+			},
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+	});
 });
--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,5 +1,12 @@
+import { HttpReponse, HttpRequest } from '../http/http';
+
 export class SignUpController {
-	handle(httRequest: any): any {
-		return { statusCode: 400, body: new Error('Missing param: name') };
+	handle(httRequest: HttpRequest): HttpReponse {
+		if (!httRequest.body.name) {
+			return { statusCode: 400, body: new Error('Missing param: name') };
+		}
+		if (!httRequest.body.email) {
+			return { statusCode: 400, body: new Error('Missing param: email') };
+		}
 	}
 }
