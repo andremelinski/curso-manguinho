b0bcec30174fbcb20e7a9fa0566e435007e8ad98
andremelinski <andremelinski29@gmail.com>
2022-12-26 23:57:49 -0300

    adding ensure requests parse as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,6 @@
-import express from 'express';
+import app from './config/app';
 
 const PORT = 5050;
-const app = express();
 
 app.listen(PORT, () => {
 	console.log(`listening at: http://localhost:${PORT}`);
