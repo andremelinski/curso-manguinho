06320d0015f6cdeccb59e2486f4ab44bc03d07a6
andremelinski <andremelinski29@gmail.com>
2023-01-04 00:55:53 -0300

    creating hash and token folder in data layer

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../interfaces/criptography/hasher.interface';
+export * from '../../interfaces/criptography/hash/hasher.interface';
 export * from '../../../domain/interfaces/model/accountModel.interfae';
 export * from '../../../domain/interfaces/usecases/addAccount.interface';
 export * from '../../interfaces/db/addAccountRepository.interface';
