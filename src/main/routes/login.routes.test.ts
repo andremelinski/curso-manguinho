1b1b21ac9f5af75cad35fbd199b342a082a92c89
andremelinski <andremelinski29@gmail.com>
2023-01-09 14:46:55 -0300

    /login logic and integratin test finished

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -1,15 +1,19 @@
 /* eslint-disable max-nested-callbacks */
+import { hash } from 'bcrypt';
+import { Collection } from 'mongodb';
 import request from 'supertest';
 
 import { MongoHelper } from '../../infra/database/mongodb/helper/mongo.helper';
 import app from '../config/app';
 
-describe('Sign up Routes', () => {
+describe('Login Routes', () => {
+	let accountCollection: Collection;
+
 	beforeAll(async () => {
 		await MongoHelper.connect(process.env.MONGO_URL);
 	});
 	beforeEach(async () => {
-		const accountCollection = await MongoHelper.getCollection('accounts');
+		accountCollection = await MongoHelper.getCollection('accounts');
 
 		await accountCollection.deleteMany({});
 	});
@@ -17,16 +21,60 @@ describe('Sign up Routes', () => {
 	afterAll(async () => {
 		await MongoHelper.disconnect();
 	});
-	test('Should return an accont on succes', async () => {
-		const accountData = {
-			name: 'valid_name',
-			email: 'valid_email@email.com',
-			password: 'valid_password',
-			passwordConfirmation: 'valid_password',
-		};
 
-		await request(app).post('/api/signup')
-			.send(accountData)
-			.expect(200);
+	describe('POST /signup', () => {
+		test('Should return 200 on sign up', async () => {
+			const accountData = {
+				name: 'valid_name',
+				email: 'valid_email@email.com',
+				password: 'valid_password',
+				passwordConfirmation: 'valid_password',
+			};
+
+			await request(app).post('/api/signup')
+				.send(accountData)
+				.expect(200);
+		});
+	});
+
+	describe('POST /login', () => {
+		test('Should return 200 on login', async () => {
+			const password = await hash('valid_password', 12);
+			const accountData = {
+				name: 'valid_name',
+				email: 'valid_email@email.com',
+				password,
+			};
+			const { email } = accountData;
+
+			await accountCollection.insertOne(accountData);
+
+			await request(app)
+				.post('/api/login')
+				.send({
+					email,
+					password: 'valid_password',
+				})
+				.expect(200);
+		});
+
+		test('Should return 401 on login', async () => {
+			const accountData = {
+				name: 'valid_name',
+				email: 'valid_email@email.com',
+				password: 'valid_password',
+			};
+			const { email, password } = accountData;
+
+			await accountCollection.insertOne(accountData);
+
+			await request(app)
+				.post('/api/login')
+				.send({
+					email,
+					password,
+				})
+				.expect(401);
+		});
 	});
 });
--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import { adaptRoute } from '../adapters/express-route.adapter';
+import { makeLoginController } from '../factories/login/login';
 import { makeSignUpController } from '../factories/sign-up/sign-up';
 
 export default (router: Router): void => {
 	router.post('/signup', adaptRoute(makeSignUpController()));
+	router.post('/login', adaptRoute(makeLoginController()));
 };
