c8e7a4502448dadf36ca5dfc33d74749060de8d4
andremelinski <andremelinski29@gmail.com>
2023-02-15 19:43:11 -0300

    test

--- src/infra/database/mongodb/helper/mongo.helper.spec.ts
+++ src/infra/database/mongodb/helper/mongo.helper.spec.ts
@@ -1,7 +1,6 @@
 /* eslint-disable max-nested-callbacks */
 import { MongoHelper as sut } from './mongo.helper';
 
-// a
 describe('Mongohelper', () => {
 	beforeAll(async () => {
 		await sut.connect(process.env.MONGO_URL);
